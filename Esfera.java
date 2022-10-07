9c5e6b9ba3f4b548a9f83a4d6a96d85526d0400a
Raíre Sampaio <114116706+rairesampaio@users.noreply.github.com>
2022-10-07 02:07:04 -0300

    Add files via upload

---
+++ Esfera.java
@@ -0,0 +1,35 @@
+Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+
+Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+Entrada
+O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
+
+Saída
+A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
+
+
+__________________________________________________
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        // ENTRADA E DECLARAÇÃO DE VARIAVEIS
+        int r = leitor.nextInt();
+        double pi = 3.14159; 
+        double R = Math.pow(r, 3);
+        double volume = ((4.0/3)*pi*R);
+        
+        // SAÍDA DOS DADOS
+        System.out.printf("VOLUME = %.3f%n",volume);
+
+        leitor.close();
+    }
+ 
+}
